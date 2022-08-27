55642b83a069c8b89cb3f3e23449e1a2e78906c7
Tomás Rodríguez <torodriguezt@unal.edu.co>
2022-08-26 19:02:35 -0500

    Taller

--- src/main/java/Taller3Java/src/taller3/televisores/TV.java
+++
@@ -1,5 +0,0 @@
-package taller3.televisores;
-
-public class TV {
-
-}
---
+++ src/main/java/taller3/televisores/TV.java
@@ -0,0 +1,92 @@
+package taller3.televisores;
+
+public class TV {
+	private Marca marca;
+	private int canal = 1; 
+	private int precio = 500;
+	private boolean estado;
+	private int volumen = 1;
+	Control control;
+	static int numTV;
+	
+	public TV(Marca marca, boolean estado) {
+		this.marca = marca;
+		this.estado = estado;
+	}
+
+	public Marca getMarca() {
+		return marca;
+	}
+
+	public void setMarca(Marca marca) {
+		this.marca = marca;
+	}
+
+	public int getCanal() {
+		return canal;
+	}
+
+	public void setCanal(int canal) {
+		this.canal = canal;
+	}
+
+	public int getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+
+	public boolean isEstado() {
+		return estado;
+	}
+
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+
+	public int getVolumen() {
+		return volumen;
+	}
+
+	public void setVolumen(int volumen) {
+		this.volumen = volumen;
+	}
+	
+	public void cantidadTV() {
+		numTV++;
+	}
+	
+	public void turnOn() {
+		estado = true;
+	}
+	
+	public void turnOff() {
+		estado = false;
+	}
+	
+	public boolean getEstado() {
+		return estado;
+	}
+	
+	public void canalUp() {
+		if (canal < 120 && estado == true)
+			canal++;
+	}
+	
+	public void canalDown() {
+		if (canal > 1 && estado == true)
+			canal--;
+	}
+	
+	public void volumenUp() {
+		if (volumen < 7 && estado == true)
+			volumen++;
+	}
+	
+	public void volumenDown() {
+		if (volumen > 0 && estado == true)
+			volumen--;
+	}
+}
